90a1859ccf11dcbd89d30bf60328dfcbf13c88de
JagadishR17K <jagadish.kollu@toucanus.com>
2023-06-15 15:37:58 +0530

    Added the history tab

--- streamlit/todo_ui.py
+++ streamlit/todo_ui.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import requests
 import json
+import pandas as pd
 from streamlit_option_menu import option_menu
 
 
@@ -82,23 +83,27 @@ if 'logged_in' in st.session_state and st.session_state['logged_in']:
         orientation="horizontal",)
 
     if selected=="To-Do":
-        task=st.text_input("Task")
-        add_button=st.button("Add Task")
-        record_data={
-            "username":username,
-            "task":task,
-            "description":"",
-            "status":"pending",
-        }
-        if add_button:
-            url=local_host+'todo/?type=insert'
-            headers = {'Authorization': f'Bearer {token}'}
-            response=requests.post(url,headers=headers,params=record_data)
-            if response.status_code==200:
-                st.write("Task added successfully")
+
+        col1,col2=st.columns(2)
+
+        with col1:
+            task=st.text_input("Task")
+            add_button=st.button("Add Task")
+            record_data={
+                "username":username,
+                "task":task,
+                "description":"",
+                "status":"pending",
+            }
+            if add_button:
+                url=local_host+'todo/?type=insert'
+                headers = {'Authorization': f'Bearer {token}'}
+                response=requests.post(url,headers=headers,params=record_data)
+                if response.status_code==200:
+                    st.write("Task added successfully")
                 
-        with st.sidebar:
-            url=local_host+'todo/?type=fetch'
+        with col2:
+            url=local_host+'todo/?type=fetch_pending'
             headers = {'Authorization': f'Bearer {token}'}
             params={
                 "username":username
@@ -110,8 +115,35 @@ if 'logged_in' in st.session_state and st.session_state['logged_in']:
                 task=record['tasklist']
                 for item in task:
                     task_button=st.checkbox(item,key=item)
+                    if task_button:
+                        description=st.text_input("Description")
+                        add_description=st.button("Add Description")
+                        if add_description:
+                            url=local_host+'todo/?type=update'
+                            headers = {'Authorization': f'Bearer {token}'}
+                            params={
+                                "username":username,
+                                "task":item,
+                                "description":description,
+                                "status":"done",
+                            }
+                            update_response=requests.post(url,headers=headers,params=params)
+                            if update_response.status_code==200:
+                                update_message=update_response.json()
+                                st.write(update_message['message'])
 
+    if selected=="History":
+        url=local_host+'todo/?type=fetch_total'
+        headers = {'Authorization': f'Bearer {token}'}
+        params={
+            "username":username
+        }
+        response=requests.get(url,headers=headers,params=params)
+        if response.status_code==200:
 
+            task_history=response.json()
+            df = pd.DataFrame(task_history)
+            df.index = [i+1 for i in range(len(df))]
+            df.index.name = 'S.No'
+            st.dataframe(df, height=350)       
 
-        
-        
